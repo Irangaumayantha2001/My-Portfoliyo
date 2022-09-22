7b4ca02c45e74de73621356ec37da4e8dd472174
Irangaumayantha2001 <iamiranga9@gmail.com>
2022-09-22 21:18:42 +0530

    add NEW SPA ASSIGNMENT

---
+++ assignment/JS/SPA/README.md
@@ -0,0 +1,23 @@
+<!-- @format -->
+
+# POS-System--Boostrap
+
+It is a combination of HTML, CSS, and Javascript code designed
+to help build user interface components.
+
+## DESIGN
+
+SITE MAP:-https://www.gloomaps.com/oaM6f9FimJ
+
+WIREFRAME:-https://wireframe.cc/pro/pp/05a282964580716
+
+MOCKUP:-//www.figma.com/file/LLrEa71M8IdET4nd6CDbHC/Pos-Sysytem?node-id=0%3A1
+
+## Technologies
+
+HTML,CSS & Boostrap
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what
+you would like to change.
---
+++ assignment/JS/SPA/assert/js/index.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+    $('#btnDashboard a').css('color', '#6c757d')
+    $('#btnCustomer a').css('color', 'black')
+    $('#btnItems a').css('color', '#6c757d')
+    $('#btnOrders a').css('color', '#6c757d')
+    $('#btnOrderDetails a').css('color', '#6c757d')
+
+    $('#dashboard').attr('style', 'display : block !important')
+    $('#customer').attr('style', 'display : none !important')
+    $('#item').attr('style', 'display : none !important')
+    $('#place-order').attr('style', 'display : none !important')
+    $('#order-details').attr('style', 'display : none !important')
+})
+
+$('#btnDashboard').click(function () {
+    $('#btnDashboard a').css('color', '#6c757d')
+    $('#btnCustomer a').css('color', 'black')
+    $('#btnItems a').css('color', '#6c757d')
+    $('#btnOrders a').css('color', '#6c757d')
+    $('#btnOrderDetails a').css('color', '#6c757d')
+
+    $('#dashboard').attr('style', 'display : block !important')
+    $('#customer').attr('style', 'display : none !important')
+    $('#item').attr('style', 'display : none !important')
+    $('#place-order').attr('style', 'display : none !important')
+    $('#order-details').attr('style', 'display : none !important')
+})
+$('#btnCustomer').click(function () {
+    $('#btnDashboard a').css('color', '#6c757d')
+    $('#btnCustomer a').css('color', 'black')
+    $('#btnItems a').css('color', '#6c757d')
+    $('#btnOrders a').css('color', '#6c757d')
+    $('#btnOrderDetails a').css('color', '#6c757d')
+
+    $('#dashboard').attr('style', 'display : none !important')
+    $('#customer').attr('style', 'display : block !important')
+    $('#item').attr('style', 'display : none !important')
+    $('#place-order').attr('style', 'display : none !important')
+    $('#order-details').attr('style', 'display : none !important')
+})
+
+$('#btnItems').click(function () {
+    $('#btnDashboard a').css('color', '#6c757d')
+    $('#btnCustomer a').css('color', '#6c757d')
+    $('#btnItems a').css('color', 'black')
+    $('#btnOrders a').css('color', '#6c757d')
+
+
+    $('#dashboard').attr('style', 'display : none !important')
+    $('#customer').attr('style', 'display : none !important')
+    $('#item').attr('style', 'display : block !important')
+    $('#place-order').attr('style', 'display : none !important')
+})
+
+$('#btnOrders').click(function () {
+    $('#btnDashboard a').css('color', '#6c757d')
+    $('#btnCustomer a').css('color', '#6c757d')
+    $('#btnItems a').css('color', '#6c757d')
+    $('#btnOrders a').css('color', 'black')
+  
+
+    $('#dashboard').attr('style', 'display : none !important')
+    $('#customer').attr('style', 'display : none !important')
+    $('#item').attr('style', 'display : none !important')
+    $('#place-order').attr('style', 'block : none !important')
+
+})
+
